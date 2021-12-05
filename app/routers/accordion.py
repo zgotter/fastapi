b5e005ac6a229856a2891a2b802d4b08de5155f0
zgotter <shkim4738@gmail.com>
2021-12-05 23:46:12 +0900

    accordion 기능 구현

--- app/routers/accordion.py
+++ app/routers/accordion.py
@@ -1,4 +1,4 @@
-from fastapi import Request, APIRouter
+from fastapi import Request, APIRouter, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -8,7 +8,16 @@ router = APIRouter()
 
 @router.get("/accordion", response_class=HTMLResponse)
 async def unsplash_home(request: Request):
+    tag = "flower"
+    result = "Type a number"
     return templates.TemplateResponse(
         "accordion.html",
-        {"request": request}
+        {"request": request, "result": result, "tag": tag}
+    )
+
+@router.post("/accordion", response_class=HTMLResponse)
+def post_accordion(request: Request, tag: str = Form(...)):
+    return templates.TemplateResponse(
+        "accordion.html",
+        {"request": request, "tag": tag}
     )
