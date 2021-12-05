b40538122160995d6283f48685987fe7b6042516
zgotter <shkim4738@gmail.com>
2021-12-05 22:31:24 +0900

    router 기능을 이용한 unsplash 페이지 구현

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from .library.helpers import *
+from app.routers import unsplash
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(unsplash.router)
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
