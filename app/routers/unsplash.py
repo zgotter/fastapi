9ab94e681b856fdad7dc97bcdbaa12bda014192f
zgotter <shkim4738@gmail.com>
2021-12-05 23:10:47 +0900

    dotenv를 활용한 설정 정보 사용

--- app/routers/unsplash.py
+++ app/routers/unsplash.py
@@ -1,6 +1,10 @@
+import os
 from fastapi import Request, APIRouter
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+
+load_dotenv()
 
 templates = Jinja2Templates(directory="templates")
 
@@ -8,6 +12,8 @@ router = APIRouter()
 
 @router.get("/unsplash", response_class=HTMLResponse)
 async def unsplash_home(request: Request):
+    key = os.getenv("unsplash_key")
+    print(key)
     return templates.TemplateResponse(
         "unsplash.html",
         {"request": request}
